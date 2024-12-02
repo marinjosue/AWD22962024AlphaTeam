7bc52ad8b712efa91ecc656f26453aa79ecdaa3a
Marin Alquinga Josue Isaac <jimarin@espe.edu.ec>
2024-12-01 19:43:01 -0500

    Implement user authentication with database connection and update login form

---
+++ 06-Code/Proyect V0.2/Connection/db.php
@@ -0,0 +1,14 @@
+<?php
+    // Conexión a la base de datos
+    $servername = "localhost";
+    $username = "root"; 
+    $password = ""; 
+    $dbname = "courses"; // Nombre de la base de datos
+
+    // Crear la conexión
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Verificar la conexión
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
+    }
+?>
