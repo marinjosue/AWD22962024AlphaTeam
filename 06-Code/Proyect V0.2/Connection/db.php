e42ab8f4706ac2fb96ff5db79f321d77393b4faf
Marin Alquinga Josue Isaac <jimarin@espe.edu.ec>
2024-12-06 16:38:45 -0500

    Update database connection details and clean up HTML files

--- 06-Code/Proyect V0.2/Connection/db.php
+++ 06-Code/Proyect V0.2/Connection/db.php
@@ -1,12 +1,12 @@
 <?php
     // Conexión a la base de datos
-    $servername = "boydpfeivqsyzxx4wzly-mysql.services.clever-cloud.com";
-    $username = "ulltzlf06xa3hjip"; 
-    $password = "n3BTQanxNlrJAlWCUlSA"; 
-    $dbname = "boydpfeivqsyzxx4wzly"; // Nombre de la base de datos
-
+    $servername = "autorack.proxy.rlwy.net";
+    $username = "root"; 
+    $password = "GdmUDNqCjqbuDPBCYwdipQHNdOgLMRim"; 
+    $dbname = "railway"; 
+    $port = "59374";
     // Crear la conexión
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname, $port);
     // Verificar la conexión
     if ($conn->connect_error) {
         die("Conexión fallida: " . $conn->connect_error);
