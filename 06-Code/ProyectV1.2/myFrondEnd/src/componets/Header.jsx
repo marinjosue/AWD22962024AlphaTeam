e67e269726ac8dd22c66e1ca352d47e4d505b935
ASUS TUF A15 <camilo_orrico@hotmail.com>
2025-02-23 16:55:59 -0500

    Index, Header

---
+++ 06-Code/ProyectV1.2/myFrondEnd/src/componets/Header.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import "../styles/header.css"; // Asegúrate de que el CSS esté en la carpeta correcta
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="navbar">
+        <div className="logo">
+          <Link to="/">
+            <img className="logo-img" src="/img/holistica_logo.png" alt="Logo" />
+          </Link>
+        </div>
+
+        <div className="hamburger" id="hamburger">
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+
+        <nav className="main-menu" id="navbar">
+          <ul>
+            <li className="menu"><Link to="/">Home</Link></li>
+            <li className="menu"><Link to="/courses">Cursos</Link></li>
+            <li className="menu"><Link to="/contact">Contacto</Link></li>
+          </ul>
+        </nav>
+
+        <div className="user-options">
+          <span id="user-links">
+            <Link to="/login" className="login-btn">Entrar</Link>
+          </span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
