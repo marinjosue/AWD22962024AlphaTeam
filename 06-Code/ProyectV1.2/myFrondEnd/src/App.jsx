cee2f70c7c113cfc22b050db3e36a9f036af0d14
ASUS TUF A15 <camilo_orrico@hotmail.com>
2025-02-23 21:53:50 -0500

    Front-end

--- 06-Code/ProyectV1.2/myFrondEnd/src/App.jsx
+++ 06-Code/ProyectV1.2/myFrondEnd/src/App.jsx
@@ -1,7 +1,55 @@
-import AppRouter from "./router";
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Importa tus vistas
+import Login from './pages/views/Login';
+import Register from './pages/views/Register';
+import UpdateUser from './pages/views/UpdateUser';
+import ViewCourses from './pages/views/ViewCourses';
+import Contact from './pages/views/Contact';
+
+// Área Admin
+import AdminProfile from './pages/admin/Profile';
+import Courses from './pages/admin/Courses';
+import CourseForm from './pages/admin/CourseForm';
+import CourseData from './pages/admin/CourseData';
+import Report from './pages/admin/Report';
+import Users from './pages/admin/Users';
+
+// Área Usuario
+import ProfileUser from './pages/user/ProfileUser';
+import CoursesEnrolled from './pages/user/CoursesEnrolled';
 
 function App() {
-  return <AppRouter />;
+  return (
+    <BrowserRouter>
+      {/* Opcional: Puedes definir un layout general con Header y Footer aquí */}
+      <Routes>
+        {/* Rutas públicas / vistas generales */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/updateUser" element={<UpdateUser />} />
+        <Route path="/viewCourses" element={<ViewCourses />} />
+        <Route path="/contact" element={<Contact />} />
+
+        {/* Rutas del área de administración */}
+        <Route path="/admin/profile" element={<AdminProfile />} />
+        <Route path="/admin/courses" element={<Courses />} />
+        <Route path="/admin/course/new" element={<CourseForm />} />
+        <Route path="/admin/course/:id" element={<CourseData />} />
+        <Route path="/admin/report" element={<Report />} />
+        <Route path="/admin/users" element={<Users />} />
+
+        {/* Rutas del área de usuario */}
+        <Route path="/user/profile" element={<ProfileUser />} />
+        <Route path="/user/coursesEnrolled" element={<CoursesEnrolled />} />
+
+        {/* Ruta por defecto */}
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- 06-Code/ProyectV1.2/myFrondEnd/src/components/Header.jsx
+++ 06-Code/ProyectV1.2/myFrondEnd/src/components/Header.jsx
@@ -1,69 +1,30 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import "../styles/header.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/header.css';
 
 const Header = () => {
-  const [userRole, setUserRole] = useState(null);
-
-  useEffect(() => {
-    const role = localStorage.getItem("userRole");
-    setUserRole(role);
-  }, []);
-
-  const renderMenu = () => {
-    if (userRole === "admin") {
-      return (
-        <ul>
-          <li className="menu"><Link to="/">Home</Link></li>
-          <li className="menu"><Link to="/manage-courses">Gestionar Cursos</Link></li>
-          <li className="menu"><Link to="/contact">Contacto</Link></li>
-          <li className="menu"><Link to="/manage-users">Gestionar Usuarios</Link></li>
-          <li className="menu"><Link to="/report">Reporte</Link></li>
-        </ul>
-      );
-    } else if (userRole === "user") {
-      return (
-        <ul>
-          <li className="menu"><Link to="/">Home</Link></li>
-          <li className="menu"><Link to="/courses">Cursos</Link></li>
-          <li className="menu"><Link to="/contact">Contacto</Link></li>
-          <li className="menu"><Link to="/my-courses">Cursos Inscritos</Link></li>
-        </ul>
-      );
-    } else {
-      return (
-        <ul>
-          <li className="menu"><Link to="/">Home</Link></li>
-          <li className="menu"><Link to="/courses">Cursos</Link></li>
-          <li className="menu"><Link to="/contact">Contacto</Link></li>
-        </ul>
-      );
-    }
-  };
-
   return (
     <header className="header">
       <div className="navbar">
         <div className="logo">
-          <Link to="/">
-            <img className="logo-img" src="/img/holistica_logo.png" alt="Logo" />
-          </Link>
+          <Link to="/"><img className="logo-img" src="/img/holistica_logo.png" alt="Logo" /></Link>
         </div>
-
-        <nav className="main-menu">
-          {renderMenu()}
+        <div className="hamburger" id="hamburger">
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+        <nav className="main-menu" id="navbar">
+          <ul>
+            <li className="menu"><Link to="/">Home</Link></li>
+            <li className="menu"><Link to="/viewCourses">Cursos</Link></li>
+            <li className="menu"><Link to="/contact">Contacto</Link></li>
+          </ul>
         </nav>
-
         <div className="user-options">
-          {userRole ? (
-            <Link to={userRole === "admin" ? "/admin-profile" : "/user-profile"} className="login-btn">
-              Perfil
-            </Link>
-          ) : (
-            <button className="login-btn" onClick={() => window.open("/login", "_blank", "width=500,height=600")}>
-              Entrar
-            </button>
-          )}
+          <span id="user-links">
+            <Link to="/login" className="login-btn">Entrar</Link>
+          </span>
         </div>
       </div>
     </header>
